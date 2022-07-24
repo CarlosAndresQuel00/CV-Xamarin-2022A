69f88967fa18c28e229a8c7b78a01feec0f4efec
carlos.quel <carlos.quel@DESKTOP-MCPUV0E>
2022-07-24 09:32:37 -0500

    Add project files.

---
+++ CV Xamarin/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CV_Xamarin.Services;
+using CV_Xamarin.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CV_Xamarin
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CV Xamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CV_Xamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CV_Xamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
