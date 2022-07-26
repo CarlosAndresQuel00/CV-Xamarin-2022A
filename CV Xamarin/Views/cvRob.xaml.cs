15dd695ecf609279dda79c90e2bb68759cfb2818
Roblink-21 <roberth.pincha@epn.edu.ec>
2022-07-25 20:57:30 -0500

    Implementacion de otra vista

---
+++ CV Xamarin/Views/cvRob.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Xamarin.Essentials;
+
+namespace CV_Xamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class cvRob : ContentPage
+    {
+        public cvRob()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            // Launch the specified URL in the system browser.
+            await Launcher.OpenAsync("https://www.linkedin.com/in/roberth-pincha-8061bb205/");
+        }
+    }
+}
