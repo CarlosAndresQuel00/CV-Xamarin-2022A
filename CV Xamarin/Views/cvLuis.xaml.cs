391542b84b59e35860eb3a1744a46055a336987f
luckLuis <jacomeluis7@gmail.com>
2022-07-26 21:00:44 -0500

    Hoja_vida_Luis_Jacome

---
+++ CV Xamarin/Views/cvLuis.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Xamarin.Essentials;
+
+namespace CV_Xamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class cvLuis : ContentPage
+    {
+        public cvLuis()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            // Launch the specified URL in the system browser.
+            await Launcher.OpenAsync("https://www.linkedin.com/in/luis-jacome-334b7219a/");
+        }
+    }
+}
